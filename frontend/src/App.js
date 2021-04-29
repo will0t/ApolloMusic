75726048fe57cf0fa1b291c789734f142a70d725
Levi <levimckenzie93@gmail.com>
2021-04-29 12:17:25 +1000

    :construction: Add backend service with Django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,3 +23,47 @@ function App() {
 }
 
 export default App;
+
+<head>
+  <script language="javascript" type="text/javascript" src="web3.min.js"></script>
+  <script language="javascript" type="text/javascript" src="cryptozombies_abi.js"></script>
+</head>
+<body>
+  <scrip>
+  window.addEventListener('load', function() {
+
+          // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+          if (typeof web3 !== 'undefined') {
+            // Use Mist/MetaMask's provider
+            web3js = new Web3(web3.currentProvider);
+          } else {
+            // Handle the case where the user doesn't have Metamask installed
+            // Probably show them a message prompting them to install Metamask
+          }
+
+          // Now you can start your app & access web3 freely:
+          startApp()
+
+        })
+
+  var mySmartContract;
+  var userAccount;
+
+  function startApp() {
+    var mySmartContractAddress = "YOUR_CONTRACT_ADDRESS";
+    mySmartContract = new web3js.eth.Contract(cryptoZombiesABI, cryptoZombiesAddress);
+
+    var accountInterval = setInterval(function() {
+      // Check if account has changed
+      if (web3.eth.accounts[0] !== userAccount) {
+        userAccount = web3.eth.accounts[0];
+        // Call a function to update the UI with the new account
+        getZombiesByOwner(userAccount)
+        .then(displayZombies);
+      }
+    }, 100);
+
+    // Start here
+  }
+  </scrip>
+</body>
