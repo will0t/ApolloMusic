82d7e3c276da5bd18d4f188cc1b84832317805ed
Kah Chun <kahl2@student.unimelb.edu.au>
2021-08-15 13:57:11 +0800

    Share Link component made and added to finishedSC

--- frontend/src/views/finishedSC.js
+++ frontend/src/views/finishedSC.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import 'antd/dist/antd.css';
+import Sharelink from '../components/sharelink.js';
 import { Button } from 'antd';
 
 class FinishedSC extends React.Component {
@@ -7,6 +8,7 @@ class FinishedSC extends React.Component {
         return (
             <div>
                 <h1>Smart Contract being deployed</h1>
+                <Sharelink/>
                 <Button href="/">
                     Click here to go back home
                 </Button>
