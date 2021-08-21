aef38481b585b418131687cc81cbcf8179183897
Zhi Wu <86734016@qq.com>
2021-08-21 19:40:10 +0800

    Update fields of the API

--- backend/Apollo_Music/SmartContract/models/SmartContract_Model.py
+++ backend/Apollo_Music/SmartContract/models/SmartContract_Model.py
@@ -17,16 +17,17 @@ class SmartContract(models.Model):
     eventName = models.CharField(max_length=60, unique=True, null=False)
     startTime = models.DateTimeField(null=False)
     duration = models.IntegerField(null=False)
-    payoutTime = models.DateTimeField(null=True)
+    payoutTime = models.DateTimeField(null=False)
     updateTime = models.DateTimeField(auto_now=True)
     createTime = models.DateTimeField(auto_now=True)
 
     #address = models.CharField(max_length=200, null=False)
-    city = models.CharField(max_length=20, null=False)
-    country = models.CharField(max_length=20, null=False)
+    #city = models.CharField(max_length=20, null=False)
+    #country = models.CharField(max_length=20, null=False)
     
     payAmount = models.DecimalField(max_digits=20, decimal_places=5, null=False)
-    
+    receiverWalletAddress = models.CharField(max_length=200, null=False)
+
     #generatedCode = models.CharField(max_length=100)
     
     #receiver = models.ManyToManyField(to="User.User")
