21ce21f58ef2be8bc5c9eb8e28f8f6c0c9a091bc
Will <weiliem.tan7@gmail.com>
2021-08-29 18:44:09 +1000

    Optimized gas fees

--- smartcontract/contracts/ApolloAgreement.sol
+++ smartcontract/contracts/ApolloAgreement.sol
@@ -7,27 +7,39 @@ pragma solidity >=0.7.0 <0.9.0;
  * @dev Manages agreements between promoters and performers for Apollo
  */
 contract ApolloAgreement {
-    
+
     struct Agreement{
         uint payoutAmount; // ERC-20/USDT implemention required
         uint payoutTime;
         address payable[] payoutDestination;
-        bool paid; // newly created agreements can replace paid agreements to save gas
+        bool paid;
     }
-    
+
     Agreement[] public agreements;
     mapping(uint => address) public agreementToPromoter;
-    mapping(address => uint32) public promoterAgreementCount; 
-    
+    mapping(address => uint32) public promoterAgreementCount;
+
     // emit events to listeners in front-end
-    event NewAgreement(uint agreementId, uint payoutAmount, uint32 payoutTime, address payable[] payoutDestination); 
+    event NewAgreement(uint agreementId, uint payoutAmount, uint32 payoutTime, address payable[] payoutDestination);
     event PaidAgreement(uint agreementId, uint payoutAmount);
 
     /**
      * @dev Create agreement between promoter and performer(s)
      */
     function createAgreement(uint32 _payoutTime, address payable[] memory _payoutDestination) public payable {
-    	// might need to check for paid agreements to replace instead of creating new agreements
+    	// check for paid agreements to replace instead of creating new agreements
+    	for (uint i = 0; i < agreements.length; i++){
+    	    if (agreements[i].paid == true){
+    	        agreements[i].payoutAmount = msg.value;
+    	        agreements[i].payoutTime = _payoutTime;
+    	        agreements[i].payoutDestination = _payoutDestination;
+    	        agreements[i].paid = false;
+    	        emit NewAgreement(i , msg.value, _payoutTime, _payoutDestination);
+    	        return;
+    	    }
+    	}
+
+    	// create new agreement in array if not found
         agreements.push(Agreement(msg.value, _payoutTime, _payoutDestination, false));
         uint id = agreements.length - 1;
         agreementToPromoter[id] = msg.sender;
@@ -49,7 +61,7 @@ contract ApolloAgreement {
         }
         return result;
     }
-      
+
     /**
      * @dev Query agreement IDs that are due for payout
      */
@@ -60,7 +72,7 @@ contract ApolloAgreement {
             size++;
           }
         }
-        
+
     	uint[] memory due = new uint[](size);
     	uint counter = 0;
     	for (uint i = 0; i < agreements.length; i++){
@@ -69,26 +81,27 @@ contract ApolloAgreement {
             counter++;
           }
         }
-        
         return due;
     }
-     
+
     /**
      * @dev Trigger payout from promoter to performer(s) by agreement ID
      */
     function payoutAgreement(uint _agreementId) public { // might change to private and implement batch payout
     	Agreement memory agreement = agreements[_agreementId];
+      require(agreement.paid == false);
     	require(agreement.payoutTime <= (block.timestamp + 86400)); // window for payout is 24 hours (86400s) after payout time
     	agreement.payoutDestination[0].transfer(agreement.payoutAmount);
+      agreements[_agreementId].paid = true;
     	emit PaidAgreement(_agreementId, agreement.payoutAmount);
     }
-    
+
     /**
      * @dev Reverse payment back to promoter before payout
      */
     function reverseAgreement() public {
     }
-     
+
     /**
     * @dev Modify agreement details
     */
